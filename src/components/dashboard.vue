a779c94500de40adf5b939597aa530fb3a4bf210
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-12-26 17:07:41 +0700

    display dashboard data from api

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -11,17 +11,119 @@
       </b-collapse>
      </b-navbar>   
      <b-container>
-      <div id="global-head">
+      <div id="global-head" class="mb-4">
        <h2>Global</h2>
-       <h4>last update: 25 Desember 2020</h4>
+       <h4>last update: <span>{{globalLastUpdate}}</span></h4>
       </div>
+      <b-row v-for="(data, idx) in apiDataGlobal" :key="idx">
+        <b-col >
+          <b-card  class="text-center">
+            <div id="card-positif">
+              <h5 class="mb-3">Positif</h5>
+              <h3 id="nilai-positif">{{data.positif}}</h3>
+            </div>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card class="text-center">
+            <div id="card-sembuh">
+              <h5 class="mb-3">Sembuh</h5>
+              <h3 id="nilai-sembuh">{{data.sembuh}}</h3>
+            </div>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card class="text-center">
+            <div id="card-meninggal">
+             <h5 class="mb-3">Meninggal</h5>
+              <h3 id="nilai-meninggal">{{data.meninggal}}</h3>
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
+      <div id="countries-head" class="mb-4">
+       <h2>Indonesia</h2>
+       <h4>last update: <span>25 Desember 2020</span></h4>
+      </div>
+      <b-row v-for="(data, idx) in apiDataIDN" :key="idx+1">
+        <b-col>
+          <b-card  class="text-center">
+            <div id="card-positif">
+              <h5 class="mb-3">Positif</h5>
+              <h3 id="nilai-positif">{{data.positif}}</h3>
+            </div>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card  class="text-center">
+            <div id="card-positif">
+              <h5 class="mb-3">Sembuh</h5>
+              <h3 id="nilai-sembuh">{{data.sembuh}}</h3>
+            </div>
+          </b-card>
+        </b-col>
+        <b-col><b-card  class="text-center">
+            <div id="card-positif">
+              <h5 class="mb-3">Meninggal</h5>
+              <h3 id="nilai-meninggal">{{data.meninggal}}</h3>
+            </div>
+          </b-card></b-col>
+      </b-row>
+      <b-button class="my-5" variant="info">More Details</b-button>
+     </b-container>
+     <b-container>
+       <div id="test">
+         <p>copyright by Farhan Rabbaanii | api using Mathdroid repository</p>
+       </div>
      </b-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  data () {
+    return {
+     apiDataGlobal: [],
+     apiDataIDN: [],
+     globalLastUpdate: null,
+     IDNLastUpdate: null
+    }
+  },
+ async created () {
+    await axios.get('https://covid19.mathdro.id/api')
+    .then((res) => {
+
+      let object = {
+        positif: res.data.confirmed.value,
+        sembuh: res.data.recovered.value,
+        meninggal: res.data.deaths.value
+      }
+      console.log(this.apiDataGlobal)
+      this.apiDataGlobal.push(object)
+      this.globalLastUpdate = res.data.lastUpdate
+    })
+    .catch((err) => {
+      alert('error when fetching API' + err)
+    })
+
+    await axios.get('https://covid19.mathdro.id/api/countries/indonesia')
+    .then((res) => {
+      let object = {
+        positif:res.data.confirmed.value,
+        sembuh: res.data.recovered.value,
+        meninggal: res.data.deaths.value
+      }
+
+      this.apiDataIDN.push(object)
+      console.log(this.apiDataIDN)
+    })
+    .catch((err) => {
+      alert('error when fetching API' + err)
+    })
+  }
 }
 </script>
 
@@ -31,9 +133,32 @@ export default {
 
 .dashboard {
   font-family: 'Roboto', sans-serif;
+  background-color: #F3F4F8;
+  color: #595E6B;
 }
 
 #global-head {
   margin-top: 100px;
+}
+
+#nilai-positif {
+  color: #DCB93D;
+}
+
+#nilai-sembuh {
+  color: #7BB87A;
+}
+
+#nilai-meninggal {
+  color: #FF7C7C;
+}
+
+#countries-head {
+  margin-top: 150px;
+}
+
+#test {
+  height: 50px;
+  background-color: #F3F4F8;
 }
 </style>
