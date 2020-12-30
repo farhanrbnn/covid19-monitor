10fede5749007115f07ccbe6740fc6ffb5121b0a
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-12-30 12:03:30 +0700

    push to branch

--- src/components/details.vue
+++ src/components/details.vue
@@ -5,7 +5,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item @click="bacot">Tentang</b-nav-item>
+        <b-nav-item @click="alertClick">Tentang</b-nav-item>
       </b-navbar-nav>
       </b-collapse>
      </b-navbar>
